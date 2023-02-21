de265577eab5ee343b9fcbd873c296d823049434
Josu3Gonher <alanjosuegonzalezhernandez@gmail.com>
2023-02-20 18:48:37 -0600

    Principios bootstrap

---
+++ unidad1/tarea1/EjerciciosTarea1/EjerciciosTarea1/EjemploForo.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosTarea1
+{
+    internal class EjemploForo
+    {
+        // Clase base abstracta para vehículos
+        abstract class Vehiculo
+        {
+            protected string marca;
+            protected string modelo;
+            protected int año;
+
+            // Propiedad para acceder a la marca del vehículo de forma encapsulada
+            public string Marca
+            {
+                get { return marca; }
+                set { marca = value; }
+            }
+
+            // Constructor para inicializar los atributos de la clase
+            public Vehiculo(string marca, string modelo, int año)
+            {
+                this.marca = marca;
+                this.modelo = modelo;
+                this.año = año;
+            }
+
+            // Método abstracto que deberá ser implementado por las clases hijas
+            public abstract void Conducir();
+
+            // Método virtual que puede ser sobrescrito por las clases hijas
+            public virtual void Reparar()
+            {
+                Console.WriteLine("El vehículo ha sido reparado.");
+            }
+        }
+
+        // Clase hija que hereda de Vehiculo y agrega funcionalidad propia
+        class Coche : Vehiculo
+        {
+            private int puertas;
+
+            // Constructor que llama al constructor de la clase base
+            public Coche(string marca, string modelo, int año, int puertas) : base(marca, modelo, año)
+            {
+                this.puertas = puertas;
+            }
+
+            // Implementación del método abstracto de la clase base
+            public override void Conducir()
+            {
+                Console.WriteLine("Conduciendo el coche de marca " + marca);
+            }
+
+            // Sobrescritura del método virtual de la clase base
+            public override void Reparar()
+            {
+                Console.WriteLine("El coche ha sido reparado.");
+            }
+        }
+
+        // Clase hija que hereda de Vehiculo y agrega funcionalidad propia
+        class Moto : Vehiculo
+        {
+            private int cilindrada;
+
+            // Constructor que llama al constructor de la clase base
+            public Moto(string marca, string modelo, int año, int cilindrada) : base(marca, modelo, año)
+            {
+                this.cilindrada = cilindrada;
+            }
+
+            // Implementación del método abstracto de la clase base
+            public override void Conducir()
+            {
+                Console.WriteLine("Conduciendo la moto de marca " + marca);
+            }
+        }
+
+        // Programa principal
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+
+                {
+                    // Crear un coche y una moto
+                    Coche coche = new Coche("Ford", "Fiesta", 2020, 5);
+                    Moto moto = new Moto("Honda", "CBR", 2021, 600);
+
+                    // Llamar a los métodos de los vehículos
+                    coche.Conducir();
+                    coche.Reparar();
+
+                    moto.Conducir();
+                    moto.Reparar();
+
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+
+}
--- unidad1/tarea1/EjerciciosTarea1/EjerciciosTarea1/Program.cs
+++ unidad1/tarea1/EjerciciosTarea1/EjerciciosTarea1/Program.cs
@@ -7,55 +7,56 @@ using System.Threading.Tasks;
 
 namespace EjerciciosTarea1
 {
-    internal class Program
+    // Programa principal
+    class Program
     {
-        static void Main(string[] args)
-        {
-            Console.Write("Que ejercicio desea ejecutar 1-11: ");
-            int eleccion = int.Parse(Console.ReadLine());
-            Console.WriteLine("");
-
-            switch (eleccion)
-            {
-                case 1:
-                    IncisoA incisoA = new IncisoA();
-                    break;
-                case 2:
-                    IncisoB incisoB = new IncisoB();
-                    break;
-                case 3:
-                    IncisoC incisoC = new IncisoC();
-                    break;
-                case 4:
-                    IncisoD incisoD = new IncisoD();
-                    break;
-                case 5:
-                    IncisoE incisoE = new IncisoE();
-                    break;
-                case 6:
-                    IncisoF incisoF = new IncisoF();
-                    break;
-                case 7:
-                    IncisoG incisoG = new IncisoG();
-                    break;
-                case 8:
-                    IncisoH incisoH = new IncisoH();
-                    break;
-                case 9:
-                    IncisoI incisoI = new IncisoI();
-                    break;
-                case 10:
-                    IncisoJ incisoJ = new IncisoJ();
-                    break;
-                case 11:
-                    IncisoK incisoK = new IncisoK();
-                    break;
-                default:
-                    Console.WriteLine("Numero ingresado invalido");
-                    break;
-            }
-            
-            Console.ReadKey();
-        }
+        
+        
     }
 }
+        //Console.Write("Que ejercicio desea ejecutar 1-11: ");
+        //int eleccion = int.Parse(Console.ReadLine());
+        //Console.WriteLine("");
+
+        //switch (eleccion)
+        //{
+        //    case 1:
+        //        IncisoA incisoA = new IncisoA();
+        //        break;
+        //    case 2:
+        //        IncisoB incisoB = new IncisoB();
+        //        break;
+        //    case 3:
+        //        IncisoC incisoC = new IncisoC();
+        //        break;
+        //    case 4:
+        //        IncisoD incisoD = new IncisoD();
+        //        break;
+        //    case 5:
+        //        IncisoE incisoE = new IncisoE();
+        //        break;
+        //    case 6:
+        //        IncisoF incisoF = new IncisoF();
+        //        break;
+        //    case 7:
+        //        IncisoG incisoG = new IncisoG();
+        //        break;
+        //    case 8:
+        //        IncisoH incisoH = new IncisoH();
+        //        break;
+        //    case 9:
+        //        IncisoI incisoI = new IncisoI();
+        //        break;
+        //    case 10:
+        //        IncisoJ incisoJ = new IncisoJ();
+        //        break;
+        //    case 11:
+        //        IncisoK incisoK = new IncisoK();
+        //        break;
+        //    default:
+        //        Console.WriteLine("Numero ingresado invalido");
+        //        break;
+        //}
+
+        
+
