1f3df0b5b56740d1f5d3b1cd9916289423eba708
Josu3Gonher <alanjosuegonzalezhernandez@gmail.com>
2023-03-29 11:14:23 -0600

    Manejo de Presupuesto con SQL

---
+++ unidad3/Manejo Presupuesto/ManejoPresupuesto/ManejoPresupuesto/Controllers/TiposCuentaController.cs
@@ -0,0 +1,23 @@
+﻿using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentaController : Controller
+    {
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+            return View();
+        }
+    }
+}
